e639d19a64665d49e4ae1b9dfe3c8356712e6e62
songa1 <songachillethe1her@gmail.com>
2023-06-13 09:18:43 +0200

    Added the text box and button

--- app/src/main/java/com/helloworld/firstapp/MainActivity.java
+++ app/src/main/java/com/helloworld/firstapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.helloworld.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button click;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
